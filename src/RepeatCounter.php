bcf09351b6edb3a83cb9cc2eda4e063c555396a7
chitra <chitra.atmakuru@gmail.com>
2015-03-13 10:28:55 -0700

    Add two tests

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -2,9 +2,14 @@
 
 class RepeatCounter
 {
-    function countRepeats
-    {
 
+    function countRepeats($string, $word)
+    {
+        $count = 0;
+        $input_str_array = explode(" ", $string);
+        if(in_array($word, $input_str_array))
+        $count++;
+        return  $count;
     }
 }
 ?>
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -12,7 +12,19 @@ class RepeatCounterTest extends PHPUnit_Framework_TestCase
         //Act
         $result = $test_RepeatCounter->countRepeats($input_string, $search_word);
         //Assert
-        $this->assertEquals(0,$reult)
+        $this->assertEquals(0,$result);
+    }
+
+    function test_letter_letter()
+    {
+        //Arrange
+        $test_RepeatCounter = new RepeatCounter;
+        $input_string = "a";
+        $search_word = "a";
+        //Act
+        $result = $test_RepeatCounter->countRepeats($input_string, $search_word);
+        //Assert
+        $this->assertEquals(1,$result);
     }
 }
 ?>
